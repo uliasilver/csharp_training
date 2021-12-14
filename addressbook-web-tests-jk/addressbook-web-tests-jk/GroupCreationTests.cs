6755fafc79152011f574cf96103d2728b99a889f
uliasilver <Julia.Kuznetsova@FLEXDATABASES.COM>
2021-12-15 00:41:44 +0300

    Задание №3

--- addressbook-web-tests-jk/addressbook-web-tests-jk/GroupCreationTests.cs
+++ addressbook-web-tests-jk/addressbook-web-tests-jk/GroupCreationTests.cs
@@ -44,10 +44,13 @@ namespace addressbook_web_tests_jk
         public void CroupCreationTests()
         {
             HomePage();
-            Login("admin", "secret");
+            Login(new AccountData ("admin", "secret"));
             GoToGroupsPage();
             InitGroupCreation();
-            FillGroupForm("aaa", "sss", "ddd");
+            GroupData group = (new GroupData("aaa");
+            group.Header = "ttt";
+            group.Footer = "ggg";
+            FillGroupForm(group);
             SubmitGroupCreation();
             ReturnToGroupsPage();
         }
@@ -63,17 +66,17 @@ namespace addressbook_web_tests_jk
             driver.FindElement(By.Name("submit")).Click();
         }
 
-        private void FillGroupForm(string name, string header, string footer)
+        private void FillGroupForm(GroupData group)
         {
             driver.FindElement(By.Name("group_name")).Click();
             driver.FindElement(By.Name("group_name")).Clear();
-            driver.FindElement(By.Name("group_name")).SendKeys(name);
+            driver.FindElement(By.Name("group_name")).SendKeys( group.Name);
             driver.FindElement(By.Name("group_header")).Click();
             driver.FindElement(By.Name("group_header")).Clear();
-            driver.FindElement(By.Name("group_header")).SendKeys(header);
+            driver.FindElement(By.Name("group_header")).SendKeys(group.Header);
             driver.FindElement(By.Name("group_footer")).Click();
             driver.FindElement(By.Name("group_footer")).Clear();
-            driver.FindElement(By.Name("group_footer")).SendKeys(footer);
+            driver.FindElement(By.Name("group_footer")).SendKeys(group.Footer);
             driver.FindElement(By.Id("content")).Click();
         }
 
@@ -87,13 +90,13 @@ namespace addressbook_web_tests_jk
             driver.FindElement(By.LinkText("groups")).Click();
         }
 
-        private void Login(string username, string password)
+        private void Login(AccountData account)
         {
             driver.FindElement(By.Name("user")).Clear();
-            driver.FindElement(By.Name("user")).SendKeys(username);
+            driver.FindElement(By.Name("user")).SendKeys(account.Username);
             driver.FindElement(By.Name("pass")).Click();
             driver.FindElement(By.Name("pass")).Clear();
-            driver.FindElement(By.Name("pass")).SendKeys(password);
+            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
             driver.FindElement(By.XPath("//input[@value='Login']")).Click();
         }
 
